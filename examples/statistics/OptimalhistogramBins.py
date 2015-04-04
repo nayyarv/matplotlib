1e49ff93f1478bf8c8c1154a29dde50656221fc1
Varun Nayyar <nayyarv@gmail.com>
2015-04-04 19:47:18 +1100

    Adding an ipython notebook to discuss the problems and some single figure comparisons

--- examples/statistics/OptimalhistogramBins.py
+++ examples/statistics/OptimalhistogramBins.py
@@ -1,3 +1,4 @@
+datsize = 10000
 __author__ = 'varunnayyar'
 
 import numpy as np
@@ -5,9 +6,9 @@ from matplotlib import pyplot as plt, mlab
 plt.style.use('ggplot')
 
 
-def min10decorator(func):
+def min6decorator(func):
     def inner(*args, **kwargs):
-        return max(func(*args, **kwargs), 10)
+        return max(func(*args, **kwargs), 6)
     return inner
 
 def sturges(x):
@@ -19,9 +20,11 @@ def rice(x):
     return np.ceil(2 * len(x) ** (1.0/3))
 
 
+def sturice(x):
+    return np.ceil((rice(x) + sturges(x))/2)
+
 def scott(x):
     h = 3.5 * x.std() * x.size **(-1.0/3)
-
     if h>0:
         return np.ceil(x.ptp()/h)
     return 1
@@ -30,7 +33,6 @@ def mad(data, axis=None):
     return np.median(np.absolute(data - np.median(data, axis)), axis)
 
 def FD(x):
-
     iqr = np.subtract(*np.percentile(x, [75, 25]))
     if iqr > 0:
         return np.ceil(x.ptp()/(2 * iqr * x.size**(-1.0/3)))
@@ -41,28 +43,32 @@ def FD(x):
 # example data
 mu = 100  # mean of distribution
 sigma = 15  # standard deviation of distribution
-x = mu + sigma * np.random.randn(10000)
+x1 = mu + sigma * np.random.randn(datsize/2)
+x2 = 80 + 5 * np.random.randn(datsize/2)
+x = np.hstack((x1, x2))
 
-autoMethods = [sturges, rice, scott, FD]
 
-num_bins = [10, 50] + map(lambda y: y(x), autoMethods)
-bindesc = ["default", "manual"] + map(lambda y: y.__name__, autoMethods)
+autoMethods = [sturges, rice, scott, sturice]
+
+num_bins = [10, 50] + map(lambda method: method(x), autoMethods)
+bindesc = ["default", "manual"] + map(lambda method: method.__name__, autoMethods)
 
 print num_bins
 print bindesc
 
+plt.figure(tight_layout=True, figsize=(14,7))
 
-exit()
-for binChoice, desc in zip(num_bins, bindesc):
+# exit()
+for i in range(len(bindesc)):
+    binChoice = num_bins[i]
+    desc = bindesc[i]
+    plt.subplot(2,3,i+1)
     # the histogram of the data
-    n, bins, patches = plt.hist(x, binChoice, normed=1, facecolor='green', alpha=0.5)
-    # add a 'best fit' line
-    y = mlab.normpdf(bins, mu, sigma)
-    plt.plot(bins, y, '--')
-    plt.xlabel('Smarts')
+    n, bins, patches = plt.hist(x, binChoice, normed=1, facecolor = 'green', alpha=0.5)
+    plt.xlabel('X axis')
     plt.ylabel('Probability')
-    plt.title(r'$N(100, 15^2)$, nbins = {}, method = {}'.format(binChoice, desc))
+    plt.title(r'method = {1}, nbins = {0}'.format(binChoice, desc))
 
     # Tweak spacing to prevent clipping of ylabel
     plt.subplots_adjust(left=0.15)
-    plt.show()
+plt.show()
